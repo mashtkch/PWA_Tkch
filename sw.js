6d277e0ff8b2e3e857871d187cfccb6579573b91
mashtkch <116003151+mashtkch@users.noreply.github.com>
2024-07-25 12:41:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,29 @@
 const staticCacheName = 's-app-v1';
 
 const assetUrls = [
+    '/',
     '/index.html',
     '/style_app.css',
     '/images',
     '/ios' 
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(staticCacheName);
-      for (const url of assetUrls) {
-        try {
-          await cache.add(url);
-          console.log(Cached asset: ${url});
-        } catch (error) {
-          console.error(Failed to cache asset: ${url}, error);
-          throw error;  // Прервать установку Service Worker, если любой запрос не удался
-        }
-      }
-    })()
-  );
-});
+self.addEventListener('install', event => { 
+    event.waitUntil( 
+      (async () => { 
+        const cache = await caches.open(staticCacheName); 
+        for (const url of assetUrls) { 
+          try { 
+            await cache.add(url); 
+            console.log(`Cached asset: ${url}`);  // Исправлено: добавлены обратные кавычки
+          } catch (error) { 
+            console.error(`Failed to cache asset: ${url}`, error);  // Исправлено: добавлены обратные кавычки
+            throw error;  // Прервать установку Service Worker, если любой запрос не удался 
+          } 
+        } 
+      })() 
+    ); 
+  });
 
 self.addEventListener('activate', event => {
   console.log('[SW]: activate');
