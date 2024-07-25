52e068aa01137f8af95e961ab4499b11d9dc5806
mashtkch <116003151+mashtkch@users.noreply.github.com>
2024-07-25 12:46:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,27 @@
-const staticCacheName = 's-app-v1';
+const staticCacheName = 'CacheSpace1';
 
 const assetUrls = [
-    '/',
-    '/index.html',
-    '/style_app.css',
-    '/images',
-    '/ios' 
-];
+  '/images',
+  '/ios',
+  '/css/styles.css',
+  '/background',
+  '/index.html',
+]
 
-self.addEventListener('install', event => { 
-    event.waitUntil( 
-      (async () => { 
-        const cache = await caches.open(staticCacheName); 
-        for (const url of assetUrls) { 
-          try { 
-            await cache.add(url); 
-            console.log(`Cached asset: ${url}`);  // Исправлено: добавлены обратные кавычки
-          } catch (error) { 
-            console.error(`Failed to cache asset: ${url}`, error);  // Исправлено: добавлены обратные кавычки
-            throw error;  // Прервать установку Service Worker, если любой запрос не удался 
-          } 
-        } 
-      })() 
-    ); 
-  });
-
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName)
+    await cache.addAll(assetUrls)
+})
 self.addEventListener('activate', event => {
   console.log('[SW]: activate');
-});
+})
 
 self.addEventListener('fetch', event => {
   console.log('Fetch', event.request.url);
   event.respondWith(cacheFirst(event.request));
-});
+})
 
 async function cacheFirst(request) {
-  const cache = await caches.open(staticCacheName);
   const cached = await cache.match(request);
   return cached ?? await fetch(request);
 }
