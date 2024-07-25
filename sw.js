53ce9337e0e68efcb2fe6272c6dbf6eb955309b9
mashtkch <116003151+mashtkch@users.noreply.github.com>
2024-07-25 12:16:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,28 @@
-const staticCacheName = 's-app-v1'
+const staticCacheName = 's-app-v1';
 
 const assetUrls = [
     'index.html',
     'style_app.css',
-    'images',
-    'ios'
-]
-
+    'images/',
+    'ios/' 
+];
 
 self.addEventListener('install', async event => {
-    const cache = await caches.open(staticCacheName)
-    await cache.addAll(assetUrls)
-})
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(staticCacheName);
+            await cache.addAll(assetUrls);
+        })()
+    );
+});
 
 self.addEventListener('fetch', event => {
-    console.log('Fetch', event.request)
+    console.log('Fetch', event.request);
 
-    event.respondWith(cacheFirst(event.request))
-})
+    event.respondWith(cacheFirst(event.request));
+});
 
 async function cacheFirst(request) {
-    const cached = await caches.match(request)
-    return cached ?? await fetch(request)
+    const cached = await caches.match(request);
+    return cached || fetch(request);
 }
